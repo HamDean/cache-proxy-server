062b926d11715ca267f438ac92be251451e8ebb0
Hamdeen <hamdeen247@gmail.com>
2026-05-25 11:21:39 +0000

    Fetch data from external API in proxy endpoint response

--- src/main/java/com/hamdeen/cachingproxyserver/controllers/ProxyController.java
+++ src/main/java/com/hamdeen/cachingproxyserver/controllers/ProxyController.java
@@ -1,15 +1,24 @@
 package com.hamdeen.cachingproxyserver.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/proxy")
 public class ProxyController {
     @GetMapping
     public ResponseEntity<?> getResource() {
-        return ResponseEntity.ok().build();
+        var restClient = RestClient.create();
+        var response = restClient.get()
+                .uri("https://jsonplaceholder.typicode.com/posts")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .body(String.class);
+
+        return ResponseEntity.ok(response);
     }
 }
